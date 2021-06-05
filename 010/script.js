36da4e9cb241b11cc97f215a5557dd6c2029a276
ononao0 <>
2021-06-05 20:59:43 +0900

    fix

--- 010/script.js
+++ 010/script.js
@@ -15,11 +15,17 @@
     let controls;
     let axesHelper;
 
-    let wingGeometry,
-        wingMaterial,
-        box,
+    let material,
+        caseMaterial,
+        wingGeometry,
+        propGeometry,
+        baseGeometry,
+        motorGeometry,
+        caseGeometry,
+        joinGeometry,
         ambientLight,
         wrapper,
+        bodyGroup,
         wingGroup,
         time = 0;
 
@@ -30,7 +36,7 @@
         far: 10000.0,
         x: 0.0,
         y: 0.0,
-        z: 35.0,
+        z: 45.0,
         lookAt: new THREE.Vector3(0.0, 0.0, 0.0),
     };
 
@@ -57,6 +63,11 @@
         color: 0xffffff
     }
 
+    const CASE_MATERIAL_PARAM = {
+        color: 0xffffff,
+        wireframe: true
+    }
+
     function init(){
         scene = new THREE.Scene();
 
@@ -76,25 +87,52 @@
         camera.lookAt(CAMERA_PARAM.lookAt);
 
         wingGroup = new THREE.Group();
+        bodyGroup = new THREE.Group();
         scene.add(wingGroup);
+        scene.add(bodyGroup);
 
-        wingMaterial = new THREE.MeshPhongMaterial(MATERIAL_PARAM);
+        const PROP_HEIGHT = 20
+
+        material = new THREE.MeshPhongMaterial(MATERIAL_PARAM);
+        caseMaterial = new THREE.MeshPhongMaterial(CASE_MATERIAL_PARAM);
+
+        propGeometry = new THREE.CylinderGeometry(0.5, 0.5, PROP_HEIGHT, 64);
+        baseGeometry = new THREE.CylinderGeometry(5, 5, 0.5, 64);
+        motorGeometry = new THREE.CylinderGeometry(2, 2, 4, 64);
+        caseGeometry = new THREE.CylinderGeometry(10, 10, 4, 64);
+        joinGeometry = new THREE.CylinderGeometry(1, 1, 4, 64);
+        const bodyMesh = new THREE.Mesh(propGeometry, material);
+        const baseMesh = new THREE.Mesh(baseGeometry, material);
+        const motorMesh = new THREE.Mesh(motorGeometry, material);
+        const joinMesh = new THREE.Mesh(joinGeometry, material);
+        const caseMesh = new THREE.Mesh(caseGeometry, caseMaterial);
+        bodyMesh.position.set(0, -PROP_HEIGHT / 2, 0)
+        baseMesh.position.set(0, -PROP_HEIGHT, 0)
+        motorMesh.rotation.x = caseMesh.rotation.x = joinMesh.rotation.x = Math.PI / 2
+        motorMesh.position.set(0, 0, 0)
+        joinMesh.position.set(0, 0, 4)
+        caseMesh.position.set(0, 0, 4)
+        scene.add(bodyMesh)
+        scene.add(baseMesh)
+        wingGroup.add(joinMesh)
+        bodyGroup.add(motorMesh)
+        bodyGroup.add(caseMesh)
         wingGeometry = new THREE.BoxGeometry(1.5, 8, 0.5);
-        const mesh1 = new THREE.Mesh(wingGeometry, wingMaterial);
-        const mesh2 = new THREE.Mesh(wingGeometry, wingMaterial);
-        const mesh3 = new THREE.Mesh(wingGeometry, wingMaterial);
-        const mesh4 = new THREE.Mesh(wingGeometry, wingMaterial);
-        mesh1.rotation.y = mesh2.rotation.y = mesh3.rotation.x = mesh4.rotation.x = 0.5
-        mesh1.position.set(0, 5, 5);
-        mesh2.position.set(0, -5, 5);
-        mesh3.rotation.z = Math.PI / 2
-        mesh4.rotation.z = Math.PI / 2
-        mesh3.position.set(-5, 0, 5);
-        mesh4.position.set(5, 0, 5);
-        wingGroup.add(mesh1);
-        wingGroup.add(mesh2);
-        wingGroup.add(mesh3);
-        wingGroup.add(mesh4);
+        const wingMesh1 = new THREE.Mesh(wingGeometry, material);
+        const wingMesh2 = new THREE.Mesh(wingGeometry, material);
+        const wingMesh3 = new THREE.Mesh(wingGeometry, material);
+        const wingMesh4 = new THREE.Mesh(wingGeometry, material);
+        wingMesh1.rotation.y = wingMesh2.rotation.y = wingMesh3.rotation.x = wingMesh4.rotation.x = 0.5
+        wingMesh1.position.z = wingMesh2.position.z = wingMesh3.position.z = wingMesh4.position.z = 4
+        wingMesh3.rotation.z = wingMesh4.rotation.z = Math.PI / 2
+        wingMesh1.position.y = 5;
+        wingMesh2.position.y = -5;
+        wingMesh4.position.x = 5;
+        wingMesh3.position.x = 5;
+        wingGroup.add(wingMesh1);
+        wingGroup.add(wingMesh2);
+        wingGroup.add(wingMesh3);
+        wingGroup.add(wingMesh4);
 
         directionalLight = new THREE.DirectionalLight(
             DIRECTIONAL_LIGHT_PARAM.color,
@@ -114,7 +152,7 @@
         // 軸ヘルパー
         axesHelper = new THREE.AxesHelper(5.0);
         scene.add(axesHelper);
-        // controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
     }
 
     function render(){
@@ -122,7 +160,8 @@
         requestAnimationFrame(render)
         wingGroup.rotation.z = -time
         wingGroup.rotation.y = Math.sin(-time / 400) * 1.5
-        // controls.update();
+        bodyGroup.rotation.y = Math.sin(-time / 400) * 1.5
+        controls.update();
         renderer.render(scene, camera);
     }
 })();
